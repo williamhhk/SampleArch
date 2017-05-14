91dcbd8c8270a98f66bbb133e7b60907f834ae76
williamhhk <william.han@gmail.com>
2017-05-14 16:55:08 -0700

    SampleArch

--- ReadMe.md
+++ ReadMe.md
@@ -6,3 +6,4 @@ http://www.zankavtaskin.com/2013/09/applied-domain-driven-design-ddd-part-1.html
 
 https://github.com/FutureProcessing/Microservices---ASP.NET-Web-API---Azure/tree/market_management_service_impl/MarketFinder.CommonHost
 
+http://www.tonytruong.net/domain-events-pattern-example/
---
+++ SampleArch.Helpers/Domain/DomainEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleArch.Helpers.Domain
+{
+    public abstract class DomainEvent
+    {
+        public string Type { get { return this.GetType().Name; } }
+
+        public DateTime Created { get; private set; }
+
+        public Dictionary<string, Object> Args { get; private set; }
+
+        public string CorrelationID { get; set; }
+
+        public DomainEvent()
+        {
+            this.Created = DateTime.Now;
+            this.Args = new Dictionary<string, Object>();
+        }
+
+        public abstract void Flatten();
+    }
+}
